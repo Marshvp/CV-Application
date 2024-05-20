9c3887b7e325ce5b5f2d47649a038c1d5be23546
Marshall Powell <marshallpowell13@gmail.com>
2024-05-20 10:08:30 +0100

    slight layout update of display

--- CV-Application/src/Components/Display.jsx
+++ CV-Application/src/Components/Display.jsx
@@ -41,33 +41,39 @@
     console.log(`props`, personal, education, experience);
 
     return (
-        <div>
-            <h1>{personal.name}</h1>
-            <h2>{personal.email}</h2>
-            <h3>{personal.phone}</h3>
-
-            <h1>Education</h1>
-            {education.map((edu, index) => (
-                <div key={index}>
-                    <h2>{edu.name}</h2>
-                    <h3>{edu.year}</h3>
-                    {edu.grades.map((grade, index) => (
-                        <div key={index}>
-                            <h4>{grade.name}</h4>
-                            <h5>{grade.grade}</h5>
-                        </div>
-                    ))}
-                </div>
-            ))}
-
-            <h1>Experience</h1>
-            {experience.map((exp, index) => (
-                <div key={index}>
-                    <h2>{exp.name}</h2>
-                    <h3>{exp.year}</h3>
-                    <p>{exp.about}</p>
-                </div>
-            ))}
+        <div className="main-Content">
+            <div className="personal">
+                <h1>{personal.name}</h1>
+                <h2>Email: {personal.email}</h2>
+                <h3>Telephone: {personal.phone}</h3>
+            </div>
+            <hr />
+            <div className="education">
+                <h1>Education</h1>
+                {education.map((edu, index) => (
+                    <div key={index}>
+                        <h2>{edu.name}</h2>
+                        <h3>{edu.year}</h3>
+                        {edu.grades.map((grade, index) => (
+                            <div key={index}>
+                                <h4>{grade.name}</h4>
+                                <h5>{grade.grade}</h5>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+                <hr />
+            <div className="experience">
+                <h1>Experience</h1>
+                {experience.map((exp, index) => (
+                    <div key={index}>
+                        <h2>{exp.name}</h2>
+                        <h3>{exp.year}</h3>
+                        <p>{exp.about}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
